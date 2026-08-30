f152efaee171e4685a1bfc44a56dde4a6987968e
syed-imadulla <imadulla.s45@gmail.com>
2026-08-30 12:40:29 +0530

    chore: remove legacy test and verification scripts

--- backend/verify.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-try:
-    # Need to get token or login, wait... admin API requires auth
-    # I can just use python to query the DB directly to mimic the API
-    pass
-except Exception as e:
-    print(e)
